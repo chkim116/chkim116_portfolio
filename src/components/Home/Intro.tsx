087fe7bf225943241c1e7c34b8521cb648bc6d99
김창회 <dudnqnfqlc@naver.com>
2021-12-30 20:16:59 +0900

    feat intro mobile

--- src/components/Home/Intro.tsx
+++ src/components/Home/Intro.tsx
@@ -1,10 +1,12 @@
 const Intro = () => {
     return (
-        <article className="w-full h-screen flex p-10 items-center bg-gray-800">
-            <div className="pl-20 pb-20">
-                <p className="text-xl text-rose-500 font-bold pb-6">Portfolio</p>
-                <h1 className="text-6xl pb-8 text-rose-700">FrontEnd Developer</h1>
-                <p className="text-lg text-gray-200">Kim Changhoe</p>
+        <article className="w-full h-screen flex sm:p-10 p-4 items-center bg-gray-800">
+            <div className="md:pl-20 sm:pl-10 pl-2 md:pb-20">
+                <p className="md:text-xl text-lg text-rose-500 font-bold pb-6">Portfolio</p>
+                <h1 className="md:text-6xl sm:text-5xl text-4xl pb-8 text-rose-700">
+                    FrontEnd Developer
+                </h1>
+                <p className="md:text-lg text-base text-gray-200">Kim Changhoe</p>
             </div>
         </article>
     );
