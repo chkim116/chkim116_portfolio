1f220802edbc91c9fa5a781f54c26af01e7c20ef
김창회 <dudnqnfqlc@naver.com>
2021-12-30 14:48:16 +0900

    feat nav

--- src/components/layouts/AppHeader.tsx
+++ src/components/layouts/AppHeader.tsx
@@ -1,12 +1,41 @@
+import { useCallback, useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
 const AppHeader = () => {
+    const [openNav, setOpenNav] = useState<'flex' | 'hidden'>('flex');
+
+    const handleOpenNav = useCallback(() => {
+        setOpenNav('flex');
+    }, []);
+
+    const handleCloseNav = useCallback(() => {
+        setOpenNav('hidden');
+    }, []);
+
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
+        <header className="w-full mx-auto h-14 flex items-center relative">
+            <button className="cursor-pointer" onClick={handleOpenNav}>
+                <AiOutlineMenu className="text-2xl" />
+            </button>
+            <nav
+                className={`w-80 h-full px-8 py-8 flex-col ${openNav} fixed top-0 left-0 bg-gray-100`}
+            >
+                <button className="max-w-fit mb-8" onClick={handleCloseNav}>
+                    <AiOutlineClose className="text-3xl" />
+                </button>
+                <ul className="w-full flex flex-col">
+                    <li className="mt-8 ml-4 ph-2 text-2xl font-thin cursor-pointer max-w-fit hover:text-orange-300">
+                        <a href="#2">About</a>
+                    </li>
+                    <li className="mt-8 ml-4 ph-2 text-2xl font-thin cursor-pointer max-w-fit hover:text-orange-300">
+                        Skills
+                    </li>
+                    <li className="mt-8 ml-4 ph-2 text-2xl font-thin cursor-pointer max-w-fit hover:text-orange-300">
+                        Projects
+                    </li>
+                    <li className="mt-8 ml-4 ph-2 text-2xl font-thin cursor-pointer max-w-fit hover:text-orange-300">
+                        Contact
+                    </li>
                 </ul>
             </nav>
         </header>
